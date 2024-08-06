f4113081d64c9033fa367c2b930ca61deb4a4a54
Dugariya <pawandugariya@gmail.com>
2024-08-06 19:02:31 +0530

    update image path

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -63,9 +63,9 @@ const ProjectSection: React.FC<ProjectSectionProps> = () => {
         "https://play.google.com/store/apps/details?id=com.sasplaygame&pcampaignid=web_share",
       githubLink: "https://github.com/example/repository",
       img: [
-        "./src/assets/sasplay.jpg",
-        "./src/assets/sasplay1.jpg",
-        "./src/assets/sasplay2.jpg",
+        "./assets/sasplay.jpg",
+        "./assets/sasplay1.jpg",
+        "./assets/sasplay2.jpg",
       ],
     },
     {
@@ -75,11 +75,7 @@ const ProjectSection: React.FC<ProjectSectionProps> = () => {
       technologies: "React Native,Expo, Redux, Firebase,",
       demoLink: "https://example.com/demo",
       githubLink: "https://github.com/example/repository",
-      img: [
-        "./src/assets/eco1.jpg",
-        "./src/assets/eco3.jpg",
-        "./src/assets/eco4.jpg",
-      ],
+      img: ["./assets/eco1.jpg", "./assets/eco3.jpg", "./assets/eco4.jpg"],
     },
     {
       title: "Free Online Plagiarism Checker for students",
@@ -88,7 +84,7 @@ const ProjectSection: React.FC<ProjectSectionProps> = () => {
       technologies: "Next-Js, Typescript, AI, node-js",
       demoLink: "https://critique.cbtexty.com/",
       githubLink: "https://github.com/example/repository",
-      img: ["./src/assets/plagicheker.png"],
+      img: ["./assets/plagicheker.png"],
     },
     // Add more project objects as needed
   ];
