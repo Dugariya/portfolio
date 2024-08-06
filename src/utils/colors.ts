36f004aa894a56c3c83be22b1172ed97190b863d
Dugariya <pawandugariya@gmail.com>
2024-08-06 17:49:14 +0530

    update the color file

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,8 +1,8 @@
-{
-  "background_color": "#012732",
-  "text_color": "#FFFFFF",
-  "button_color": "#FFA500",
-  "shadow_color": "#024265",
-  "highlight_color": "#D4AF37",
-  "link_color": "#00BFFF"
-}
+let color = {
+  background_color: "#012732",
+  text_color: "#FFFFFF",
+  button_color: "#FFA500",
+  shadow_color: "#024265",
+  highlight_color: "#D4AF37",
+  link_color: "#00BFFF",
+};
